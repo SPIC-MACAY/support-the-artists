3080ae49957f074f78b443f20b325889715cb52a
Ankit Babbar <ankit@spicmacay.com>
2021-08-11 17:30:27 +0530

    Added Sample Image
    Signed-off-by: Ankit Babbar <ankit@spicmacay.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import  groupBy from "lodash.groupby";
+
 export async function getServerSideProps() {
   let apiEndPoint = "http://localhost:3000/api/support-the-artists";
   const res = await fetch(apiEndPoint);
@@ -14,7 +16,7 @@ export interface ArtistData {
   youTubeLink: string;
 }
 
-const getArtistDataFromData = (data: any): ArtistData => {
+export const getArtistDataFromData = (data: any): ArtistData => {
   // const sampleData = {"date":"Date(2021,5,3)","episode":1," name":"dayam khan","mobile number":9024005284,"art form":"rajasthani folk","category":"rajasthan","state":"no-044710065357","account number":"IPOS0000001","IFSC code":"india post payment bank","bank":"siff academy","branch":"https://youtu.be/xXd7Nb9O3ps"}
   return {
     name: data[" name"],
@@ -31,10 +33,24 @@ const sampleData: ArtistData[] = [
     mobileNumber: "9024005284",
     youTubeLink: "",
   },
+  {
+    name: "dayam khan2",
+    artForm: "rajasthani folk",
+    mobileNumber: "9024005284",
+    youTubeLink: "",
+  },
+  {
+    name: "dayam khan",
+    artForm: "kalighat painting",
+    mobileNumber: "9024005284",
+    youTubeLink: "",
+  },
 ];
 
 export default function Home({ data }: { data: any[] }) {
-
   //const structuredData = data.map((row) => getArtistDataFromData(row));
-  return <ul>{JSON.stringify(sampleData)}</ul>;
+  const artFormGroupData = groupBy(sampleData, 'artForm');
+  console.log(artFormGroupData);
+  return <p><ul>{JSON.stringify(sampleData)}</ul>
+  <img src="sample-image.jpeg" /></p>;
 }
