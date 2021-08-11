6044bb25318223a03eca573a0fd20f292bafe2c2
Ankit Babbar <ankit@spicmacay.com>
2021-08-11 17:23:01 +0530

    Added API enhancemenets
    Signed-off-by: Ankit Babbar <ankit@spicmacay.com>

--- pages/api/support-the-artists.ts
+++ pages/api/support-the-artists.ts
@@ -1,3 +1,24 @@
-export default function handler(_: any, res: any) {
-  res.status(200).json({ name: 'John Doe' })
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
+
+export default async function handler(_: any, res: any) {
+  try {
+    const spreadsheetId = process.env.SUPPORT_THE_ARTISTS_SPREADSHEET_ID;
+    const sheetName = process.env.SUPPORT_THE_ARTISTS_SHEET_NAME;
+    const parser = new PublicGoogleSheetsParser(spreadsheetId);
+    return new Promise<void>((resolve, reject) => {
+      parser
+        .parse(spreadsheetId, sheetName)
+        .then((items: any) => {
+          res.status(200).json(items);
+          res.end();
+          resolve();
+        })
+        .catch((err: Error) => {
+          reject(err);
+          console.error(err);
+        });
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 export async function getServerSideProps() {
-  let apiEndPoint = 'http://localhost:3000/api/support-the-artists';
+  let apiEndPoint = "http://localhost:3000/api/support-the-artists";
   const res = await fetch(apiEndPoint);
   const data = await res.json();
   return {
-    props: data, // will be passed to the page component as props
+    props: { data }, // will be passed to the page component as props
   };
 }
 
-export default function Home(googleSheetData: any) {
-  console.log(googleSheetData);
-  return (
-    <ul>
-      {JSON.stringify(googleSheetData)}
-    </ul>
-  );
+export interface ArtistData {
+  name: string;
+  artForm: string;
+  mobileNumber: string;
+  youTubeLink: string;
+}
+
+const getArtistDataFromData = (data: any): ArtistData => {
+  // const sampleData = {"date":"Date(2021,5,3)","episode":1," name":"dayam khan","mobile number":9024005284,"art form":"rajasthani folk","category":"rajasthan","state":"no-044710065357","account number":"IPOS0000001","IFSC code":"india post payment bank","bank":"siff academy","branch":"https://youtu.be/xXd7Nb9O3ps"}
+  return {
+    name: data[" name"],
+    artForm: data["art form"],
+    mobileNumber: data["mobile number"],
+    youTubeLink: "",
+  };
+};
+
+const sampleData: ArtistData[] = [
+  {
+    name: "dayam khan",
+    artForm: "rajasthani folk",
+    mobileNumber: "9024005284",
+    youTubeLink: "",
+  },
+];
+
+export default function Home({ data }: { data: any[] }) {
+
+  //const structuredData = data.map((row) => getArtistDataFromData(row));
+  return <ul>{JSON.stringify(sampleData)}</ul>;
 }
